d43295d7b637b3301703e71cd82814f19becad7b
as1-2-bidama <sa080201@icloud.com>
2024-07-18 16:42:20 +0900

    liquidation

--- setting.py
+++
@@ -1,4 +0,0 @@
-screen_width = 1200
-screen_height = 900
-color_list = {"white":(255,255,255), "black":(0,0,0), "silver":(192,192,192), "gray":(128,128,128), "red":(255,0,0), "maroon":(128,0,0), "yellow":(255,255,0), "olive":(128,128,0), "rime":(0,255,0), "greem":(0,128,0), "aqua":(0,255,255), "teal":(0,128,128), "blue":(0,0,255), "navy":(0,0,128), "fuchusia":(255,0,255), "purple":(128,0,128)}
-fps = 60
